c6ceb665d68668cfcc897699cb79c740e51c207c
daemtri <daemtri-molly@bianfeng.com>
2023-06-06 11:53:06 +0800

    支持优雅退出

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use std::error::Error;
 use std::net::SocketAddr;
 use std::sync::{Arc, Mutex};
 use tokio::net::TcpListener;
-
+use tokio::signal;
 use tungstenite::protocol::Message;
 
 mod gate;
@@ -23,10 +23,20 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
     let transmitter = Arc::new(gate::transmit::Transmitter::new());
     loop {
-        let (tcp_stream, socket_addr) = lis.accept().await?;
-        let transmitter = transmitter.clone();
-        tokio::spawn(async move {
-            multiplier::handle_tcp_stream(transmitter, tcp_stream, socket_addr).await;
-        });
+        tokio::select! {
+            result = lis.accept() => {
+                let (tcp_stream, socket_addr) = result.unwrap();
+                let transmitter = transmitter.clone();
+                tokio::spawn(async move {
+                    multiplier::handle_tcp_stream(transmitter, tcp_stream, socket_addr).await;
+                });
+            }
+            _ = signal::ctrl_c() => {
+                log::info!("应用退出");
+                break;
+            }
+        }
     }
+
+    Ok(())
 }
