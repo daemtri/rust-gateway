d72d959db396c988cfe46e28d459ab64f963b562
daemtri <daemtri-molly@bianfeng.com>
2023-06-06 12:58:54 +0800

    add tcp_serve

--- src/gate/mod.rs
+++ src/gate/mod.rs
@@ -1,3 +1,19 @@
+use std::sync::Arc;
+use tokio::net::TcpListener;
+
 pub mod handle;
 pub mod multiplier;
 pub mod transmit;
+
+pub async fn tcp_serve(
+    lis: TcpListener,
+    transmitter: Arc<transmit::Transmitter>,
+) -> anyhow::Result<()> {
+    loop {
+        let (tcp_stream, socket_addr) = lis.accept().await.unwrap();
+        let transmitter = transmitter.clone();
+        tokio::spawn(async move {
+            multiplier::handle_tcp_stream(transmitter, tcp_stream, socket_addr).await;
+        });
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use futures_channel::mpsc::UnboundedSender;
-use gate::multiplier;
 use std::collections::HashMap;
 use std::error::Error;
 use std::net::SocketAddr;
@@ -20,23 +19,15 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let addr = "0.0.0.0:8080";
     let lis = TcpListener::bind(addr).await.unwrap();
     log::info!("tcp listen on {}", addr);
-
     let transmitter = Arc::new(gate::transmit::Transmitter::new());
-    loop {
-        tokio::select! {
-            result = lis.accept() => {
-                let (tcp_stream, socket_addr) = result.unwrap();
-                let transmitter = transmitter.clone();
-                tokio::spawn(async move {
-                    multiplier::handle_tcp_stream(transmitter, tcp_stream, socket_addr).await;
-                });
-            }
-            _ = signal::ctrl_c() => {
-                log::info!("应用退出");
-                break;
-            }
+
+    tokio::select! {
+        _ = gate::tcp_serve(lis,transmitter) => {
+            Ok(())
+        }
+        _ = signal::ctrl_c() => {
+            log::info!("应用退出");
+            Ok(())
         }
     }
-
-    Ok(())
 }
